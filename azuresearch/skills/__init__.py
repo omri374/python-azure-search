a644d523f491a0be7fe20c4cb83ad27a31b5e014
Omri Mendels <omri.mendels@microsoft.com>
2019-01-13 11:55:34 +0200

    package update

--- azuresearch/skills/__init__.py
+++ azuresearch/skills/__init__.py
@@ -1,2 +1,5 @@
-from .skill import Skill,SkillInput,SkillOutput
+from .predefined.cognitive_skills import SplitSkill, KeyPhraseExtractionSkill, EntityRecognitionSkill, \
+    ImageAnalysisSkill, LanguageDetectionSkill, MergeSkill, OCRSkill, SentimentSkill, ShaperSkill, predefined_skills
+from .skill import Skill, SkillInput, SkillOutput
 from .skillset import Skillset
+from .web_api_skill import WebApiSkill
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='python-azure-search',
-    version='',
+    name='azure_search',
+    version='0.0.1',
     packages=['azuresearch', 'azuresearch.indexes', 'azuresearch.skills', 'azuresearch.analyzers'],
-    url='',
-    license='',
-    author='',
-    author_email='',
-    description='',
+    url='https://github.com/omri374/python-azure-search',
+    license='MIT',
+    author='Samuel Spencer, Omri Mendels, Elad Iwanir',
+    author_email='omri.mendels@microsoft.com',
+    description='Python package for calling Azure Search and Azure Cognitive Search',
     install_requires=['requests'],
     test_requires=['haikunator', 'httpretty']
 )
